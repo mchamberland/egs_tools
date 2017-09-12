e1c173818ed0b8396184b0040f942124c0af260e
Marc Chamberland <mchamberland@mac.com>
2017-09-12 06:19:18 -0700

    WIP dicom info parser
    Former-commit-id: 59f2495cf21a6689e983b80d93a0bec10b104a65
    Former-commit-id: a8a7d293139059a78bcdbecfb44ab1506ea1225f
    Former-commit-id: e2755aad3e408350738a46e5ebe9586d2d3e1eda

--- brachydicom.py
+++ brachydicom.py
@@ -3,47 +3,61 @@ from os.path import join, isfile, splitext
 import pydicom
 
 
-def get_dwell_positions_and_times():
+def get_all_files_in_directory(directory='.'):
+    return [f for f in listdir(directory) if isfile(join(directory, f))]
 
-    all_files = [f for f in listdir('.') if isfile(join('.', f))]
-    dicom_files = [f for f in all_files if f.endswith('.dcm')]
-    plan_files = [f for f in dicom_files if f.startswith('RP')]
 
-    for file in plan_files:
-        dc = pydicom.read_file(file)
-        root = dc.ApplicationSetupSequence[0].ChannelSequence
-        length = len(root)
-        dwell_filename = splitext(file)[0] + '.dwell'
+def get_dicom_files_in_directory(directory='.'):
+    all_files = get_all_files_in_directory(directory)
+    return [f for f in all_files if f.endswith('.dcm')]
 
-        with open(dwell_filename, 'wt') as dwell_file:
-            dwell_pos_and_times = ''
 
-            # TODO should take all dwell positions to determine direction cosines and, hence, rotation of source!
-            for i in range(length):
-                number_of_control_points = int(root[i].NumberOfControlPoints)
-                channel_total_time = float(root[i].ChannelTotalTime)
-                final_cumulative_time_weight = float(root[i].FinalCumulativeTimeWeight)
-                ctrl_pts = root[i].BrachyControlPointSequence
+def get_plan_files_in_directory(directory='.'):
+    dicom_files = get_dicom_files_in_directory(directory)
+    return [f for f in dicom_files if f.startswith('RP')]
 
-                current_time = 0
-                current_position = [-9999, -9999, -9999]
 
-                for j in range(number_of_control_points):
-                    previous_position = current_position
-                    current_position = list(map(float, ctrl_pts[j].ControlPoint3DPosition))
+def get_structure_files_in_directory(directory='.'):
+    dicom_files = get_dicom_files_in_directory(directory)
+    return [f for f in dicom_files if f.startswith('RS')]
 
-                    previous_time = current_time
-                    cumulative_time_weight = float(ctrl_pts[j].CumulativeTimeWeight)
-                    current_time = channel_total_time * cumulative_time_weight / final_cumulative_time_weight
 
-                    # if the positions are different or the times are identical, then the source is in transit
-                    if current_position != previous_position or current_time == previous_time:
-                        pass
-                    # this is a dwell position
-                    else:
-                        time = current_time - previous_time
-                        x, y, z = current_position
-                        dwell_pos_and_times +=\
-                            '{0:.2f}\t{1:.2f}\t{2:.2f}\t{3:.2f}\n'.format(x / 10, y / 10, z / 10, time)
+def get_dose_files_in_directory(directory='.'):
+    dicom_files = get_dicom_files_in_directory(directory)
+    return [f for f in dicom_files if f.startswith('RD')]
 
-            dwell_file.write(dwell_pos_and_times)
+
+def open_dicom_files(dicom_files):
+    filenames = [splitext(file)[0] for file in dicom_files]
+    dicom_files = [pydicom.read_file(file) for file in dicom_files]
+    return filenames, dicom_files
+
+
+def get_dwells(dicom_file):
+    root = dicom_file.ApplicationSetupSequence[0].ChannelSequence
+    number_of_channels = len(root)
+    dwell_positions = []
+    dwell_times = []
+
+    for i in range(number_of_channels):
+        number_of_control_points = int(root[i].NumberOfControlPoints)
+        channel_total_time = float(root[i].ChannelTotalTime)
+        final_cumulative_time_weight = float(root[i].FinalCumulativeTimeWeight)
+        ctrl_pts = root[i].BrachyControlPointSequence
+
+        current_time = 0
+        current_position = [-9999, -9999, -9999]
+
+        for j in range(number_of_control_points):
+            previous_position = current_position
+            current_position = list(map(float, ctrl_pts[j].ControlPoint3DPosition))
+
+            previous_time = current_time
+            cumulative_time_weight = float(ctrl_pts[j].CumulativeTimeWeight)
+            current_time = channel_total_time * cumulative_time_weight / final_cumulative_time_weight
+
+            if current_position == previous_position:
+                dwell_positions.append(current_position)
+                dwell_times.append(current_time - previous_time)
+
+    return dwell_positions, dwell_times, number_of_channels
