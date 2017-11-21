fa3abc4231b0a286fa72f0b717b887f208e15d50
Marc Chamberland <mchamberland@mac.com>
2017-11-21 07:20:51 -0800

    WIP on ct_tools
    Former-commit-id: 898a6a7df5725d32cd84c20c22856e6519c9f78b
    Former-commit-id: 43ea65a1193596c8d7803a0448906409e349c2c5
    Former-commit-id: 75d0ac2c8bb8039b6c9409ab5fe497b7db1be40d

--- brachy_dicom/reader.py
+++ brachy_dicom/reader.py
@@ -38,6 +38,22 @@ def list_ct_files_in_directory(directory='.'):
     return [f for f in dicom_files if pydicom.read_file(join(directory, f)).Modality == 'CT']
 
 
+def read_ct_files_in_directory(directory='.'):
+    files = list_ct_files_in_directory(directory)
+    filenames = [splitext(file)[0] for file in files]
+    ct_files = [pydicom.read_file(join(directory, file)) for file in files]
+    return filenames, ct_files
+
+
+def get_sorted_ct_files_in_directory(directory='.'):
+    # returns a list of tuples (z-slice in mm, CT filename, Pydicom dataset), sorted by z-slice
+    filenames, ct_files = read_ct_files_in_directory(directory)
+    zslices = [float(ct.ImagePositionPatient[2]) for ct in ct_files]
+    temp = list(zip(zslices, filenames, ct_files))
+    temp.sort()
+    return temp
+
+
 def read_plan_files_in_directory(directory='.'):
     files = list_plan_files_in_directory(directory)
     filenames = [splitext(file)[0] for file in files]
@@ -59,7 +75,7 @@ def read_structure_files_in_directory(directory='.'):
     return filenames, dicom_structs
 
 
-def get_all_dwells(dicom_file) -> Tuple[np.ndarray, np.ndarray, int]:
+def get_all_dwells_in_mm_and_s(dicom_file) -> Tuple[np.ndarray, np.ndarray, int]:
     # these include dwells that have no source, to use possibly to determine source orientation in 3D
     # positions in mm and times in s
     root = dicom_file.ApplicationSetupSequence[0].ChannelSequence
@@ -91,9 +107,9 @@ def get_all_dwells(dicom_file) -> Tuple[np.ndarray, np.ndarray, int]:
     return np.array(dwell_positions), np.array(dwell_times), number_of_channels
 
 
-def get_dwells(dicom_file) -> Tuple[np.ndarray, np.ndarray, int]:
+def get_dwells_in_mm_and_s(dicom_file) -> Tuple[np.ndarray, np.ndarray, int]:
     # positions in mm and times in s
-    dwells, times, number_of_channels = get_all_dwells(dicom_file)
+    dwells, times, number_of_channels = get_all_dwells_in_mm_and_s(dicom_file)
     dwell_times = times[times > 0]
     dwell_positions = dwells[times > 0]
     return dwell_positions, dwell_times, number_of_channels
@@ -111,12 +127,12 @@ def get_treatment_technique(dicom_file):
     return dicom_file.BrachyTreatmentTechnique
 
 
-def get_rakr(dicom_file):
+def get_rakr_in_ugy_per_h_at_1m(dicom_file):
     # in microGy per hour at 1 m
     return float(dicom_file.SourceSequence[0].ReferenceAirKermaRate)
 
 
-def get_half_life(dicom_file):
+def get_half_life_in_days(dicom_file):
     # in days
     return float(dicom_file.SourceSequence[0].SourceIsotopeHalfLife)
 
@@ -138,9 +154,9 @@ def get_study_date_and_time(dicom_file):
         return datetime.datetime.strptime(date_time, "%Y%m%d%H%M%S")
 
 
-def get_total_treatment_time(dicom_file):
+def get_total_treatment_time_in_s(dicom_file):
     # in seconds
-    rakr = get_rakr(dicom_file)
+    rakr = get_rakr_in_ugy_per_h_at_1m(dicom_file)
     total_reference_air_kerma = float(dicom_file.SourceSequence[0].ApplicationSetupSequence[0].TotalReferenceAirKerma)
     return total_reference_air_kerma / rakr * 3600
 
@@ -153,7 +169,7 @@ def get_number_of_seeds(dicom_file):
     return len(dicom_file.ApplicationSetupSequence)
 
 
-def get_seed_locations(dicom_file) -> np.ndarray:
+def get_seed_locations_in_mm(dicom_file) -> np.ndarray:
     # positions in mm
     seed_positions = []
 
---
+++ ct_tools/ctdata.py
@@ -0,0 +1,75 @@
+"""ctdata
+
+This module exports classes to handle CT image data.
+
+class:
+
+CTframe -- holds a single x-y CT slice as a flattened list
+CTdata -- a CTdata object
+"""
+import numpy as np
+from typing import List
+
+
+class CTframe:
+    def __init__(self, ct_file):
+        self.number_of_rows = ct_file.Rows
+        self.number_of_columns = ct_file.Columns
+        self.row_spacing_in_cm = float(ct_file.PixelSpacing[0]) / 10
+        self.column_spacing_in_cm = float(ct_file.PixelSpacing[1]) / 10
+        self.image_position_in_cm = [float(x) / 10 for x in ct_file.ImagePositionPatient]
+        self.rescale_intercept = float(ct_file.RescaleIntercept)
+        self.rescale_slope = float(ct_file.RescaleSlope)
+
+        self.flat_raw_values = []
+        for y in range(self.number_of_rows):
+            for x in range(self.number_of_columns):
+                self.flat_raw_values.append(ct_file.pixel_array[y][x])
+
+
+class CTdata:
+    def __init__(self, ctframes: List[CTframe]):
+        self.dimensions = [ctframes[0].number_of_columns, ctframes[0].number_of_rows, len(ctframes)]
+        self.bounds = self.get_bounds(ctframes)
+        self.image = np.array([])
+        self.get_ct_data(ctframes)
+
+    @staticmethod
+    def get_bounds(ctframes: List[CTframe]):
+        xbounds = []
+        ybounds = []
+        zbounds = []
+
+        delta_z = ctframes[1].image_position_in_cm[2] - ctframes[0].image_position_in_cm[2]
+
+        for frame in ctframes:
+            delta_x = frame.column_spacing_in_cm
+            delta_y = frame.row_spacing_in_cm
+
+            xbounds.append(frame.image_position_in_cm[0] - delta_x / 2)
+            ybounds.append(frame.image_position_in_cm[1] - delta_y / 2)
+            zbounds.append(frame.image_position_in_cm[2] - delta_z / 2)
+
+            for i in range(frame.number_of_columns):
+                xbounds.append(xbounds[i] + delta_x)
+
+            for j in range(frame.number_of_rows):
+                ybounds.append(ybounds[j] + delta_y)
+
+            for k in range(len(ctframes)):
+                zbounds.append(zbounds[k] + delta_z)
+
+        return [xbounds, ybounds, zbounds]
+
+    def get_ct_data(self, ctframes: List[CTframe]):
+        for frame in ctframes:
+            temp = np.array(frame.flat_raw_values) * frame.rescale_slope + frame.rescale_intercept
+            self.image = np.append(self.image, temp)
+        self.image = self.image.reshape(self.dimensions, order='F')
+
+    def nvox(self) -> int:
+        """Return the total number of voxels"""
+        nvox = 1
+        for dims in self.dimensions:
+            nvox *= dims
+        return nvox
