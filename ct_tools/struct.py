d845f93cd56c38be3df22a21865569ab9340dada
Marc Chamberland <mchamberland@mac.com>
2017-11-29 19:12:33 -0800

    Add interpolation of missing contour slices
    Former-commit-id: 468a0ff091f65cc51494268fff45d3beb90aef68
    Former-commit-id: 848b5205f645e7b2d3f6db1c4b63b2d6dc3fcd15
    Former-commit-id: 12619d7a1f799d8e7c1e1d3bdd003fbe97bda988

--- ct_tools/struct.py
+++ ct_tools/struct.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.spatial import cKDTree
+from matplotlib.path import Path
 import brachy_dicom.reader as bdr
 
 
@@ -11,7 +13,7 @@ def get_contours_from_dicom(directory='.'):
     contour_list = []
     for ROI in struct.RTROIObservationsSequence:
         if ROI.RTROIInterpretedType == "BRACHY_CHANNEL":
-            continue
+            continue  # don't know what to do with those yet...
 
         label = ROI.ROIObservationLabel
         number = ROI.ReferencedROINumber
@@ -30,7 +32,7 @@ def get_contours_from_dicom(directory='.'):
             contour_data.append(list(zip(x, y)))
             zslices.append(z[0])
 
-        contour_list.append(Contour(np.array(contour_data), np.array(zslices), label, number, colour))
+        contour_list.append(Contour(contour_data, np.array(zslices), label, number, colour))
 
     return contour_list
 
@@ -47,22 +49,62 @@ class Contour:
 
     def is_missing_slices(self, slice_thickness):
         slice_differences = np.diff(self.zslices) / slice_thickness
-        missing_slices = slice_differences - 1
-        total_number_of_missing_slices = int(round(missing_slices.sum()))
+        missing_slices = np.around(slice_differences - 1, decimals=8)
+        total_number_of_missing_slices = int(missing_slices.sum())
         if total_number_of_missing_slices > 0:
-            print("Missing {} slice(s) in contour {}".format(total_number_of_missing_slices, self.name))
+            print("Missing {} slice(s) in contour {}:".format(total_number_of_missing_slices, self.name))
             self.missing_slices = []
-            for index in np.where(missing_slices > 0):
-                self.missing_slices.append(MissingSlices(self.zslices[index][0], self.zslices[index+1][0],
-                                                         int(missing_slices[index][0]), slice_thickness))
+            for index in np.where(missing_slices > 0)[0]:
+                print("Missing {} slice(s) between z = {} and z = {}".format(int(missing_slices[index]),
+                                                                             self.zslices[index],
+                                                                             self.zslices[index + 1]))
+                self.missing_slices.append(MissingSlices(index, int(missing_slices[index]), slice_thickness))
+            print("")
             return True
         else:
             return False
 
+    def interpolate_missing_slices(self):
+        if not self.missing_slices:
+            print("Contours are not missing slices. Nothing to do here!")
+        else:
+            print("Interpolating missing slices...")
+            for (i, missing_slice_group) in enumerate(self.missing_slices):
+                start = missing_slice_group.start_index
+                start_slice = np.array(self.contour_data[start])
+                end_slice = np.array(self.contour_data[start + 1])
+
+                # find nearest neighbours of the end slice in the first slice
+                distances, indices = cKDTree(start_slice).query(end_slice)
+                point_pairs = list(zip(start_slice[indices], end_slice))
+                zstart = self.zslices[start]
+                zend = self.zslices[start + 1]
+
+                number_missing = missing_slice_group.number_of_missing_slices
+
+                for n in range(number_missing):
+                    zslice = self.zslices[start] + ((n + 1) * missing_slice_group.thickness)
+                    interpolated_xy = []
+                    for (start_xy, end_xy) in point_pairs:
+                        x1, y1 = start_xy
+                        x2, y2 = end_xy
+                        x = (x1 * (zend - zslice) + x2 * (zslice - zstart)) / (zend - zstart)
+                        y = (y1 * (zend - zslice) + y2 * (zslice - zstart)) / (zend - zstart)
+                        interpolated_xy.append((x, y))
+                    self.zslices = np.append(self.zslices, zslice)
+                    self.contour_data.append(interpolated_xy)
+
+            print("...Done! Sorting the newly added slices...")
+            temp = list(zip(self.contour_data, self.zslices))
+            temp.sort(key=lambda f: f[1])
+            self.contour_data, self.zslices = zip(*temp)
+            self.missing_slices = None
+            self.is_interpolated = True
+            print("...Sorted!")
+
 
 class MissingSlices:
-    def __init__(self, start, end, number, thickness):
-        self.start_slice = start
-        self.end_slice = end
+    def __init__(self, start, number, thickness):
+        self.start_index = start
         self.number_of_missing_slices = number
         self.thickness = thickness
