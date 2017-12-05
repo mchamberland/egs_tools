c0e0504f7ef33a5b029ad2c8b95b946336dede5f
Marc Chamberland <mchamberland@mac.com>
2017-12-04 18:11:24 -0800

    WIP on CT to tissue conversion
    Former-commit-id: d55f61ca013b71fd9e468c6321a81f4db120e7ad
    Former-commit-id: fe47f0ab5d94048db195c0dc8cb53600da6c2793
    Former-commit-id: 2e46f5799234e5bfa63dd0b59e1723762982fd88

--- ct_tools/ct_to_tissue.py
+++ ct_tools/ct_to_tissue.py
@@ -1,27 +1,45 @@
 import os
+import numpy as np
+from os.path import join, isfile, splitext
 
 
 class CTConversionToTissue:
-    def __init__(self, directory="."):
+    def __init__(self, filename=None, directory="."):
         self.directory = directory
         self.media_list = []
+        self.ctnum_bins = []
+        if filename:
+            self.read_ctconv_file(filename)
 
     def read_ctconv_file(self, filename='default.ctconv'):
-        if not os.path.exists(filename):
+        path = join(self.directory, filename)
+        if not os.path.exists(path):
             return -1
 
-        with open(filename, 'r') as file:
+        with open(path, 'r') as file:
             lines = file.readlines()
             lines = [line.strip() for line in lines if line.strip()]
 
         for line in lines:
-            name, density, min_ctnum, max_ctnum = [x for x in line]
+            name, density, min_ctnum, max_ctnum = line.split()
             medium = MediumInfo(name, float(density), int(min_ctnum), int(max_ctnum))
             self.media_list.append(medium)
+            self.ctnum_bins.append(min_ctnum)
+
+        self.ctnum_bins.append(self.media_list[-1].max_ctnum)
+
+    def get_medium_density(self, index):
+        return self.media_list[index].density
+
+    def get_medium_index_from_ctnum(self, ctnum):
+        return np.searchsorted(self.ctnum_bins, ctnum) - 1
+
+    def get_medium_name_from_ctnum(self, ctnum):
+        return self.media_list[np.digitize(ctnum, self.ctnum_bins)[0]].name
 
 
 class MediumInfo:
-    def __init__(self, name=None, density=None, min_ctnum=-1000, max_ctnum=3000):
+    def __init__(self, name='WATER_0.998', density=0.998, min_ctnum=-9999, max_ctnum=9999):
         self.name = name
         self.density = density
         self.min_ctnum = min_ctnum
