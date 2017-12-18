3f38d9587b6d790a49a7cdfff38ad956b2307985
Marc Chamberland <mchamberland@mac.com>
2017-12-18 12:15:23 -0800

    Use cylindrical search around seeds for STR
    Former-commit-id: 0dc238a56cd18de7f8a4552d35d76756f8a61c4a
    Former-commit-id: 9191b40f060851f36ffb02315ab22b5251cf4fff
    Former-commit-id: 4ea4bb2d066efba9ff8cbcb4b4864a42eef4d391

--- ct_tools/artifact.py
+++ ct_tools/artifact.py
@@ -1,3 +1,4 @@
+import math
 import voxelnav
 from typing import List
 from ct_tools.ctdata import CTdata
@@ -5,24 +6,30 @@ from ct_tools.ctdata import CTdata
 
 class SimpleThresholdReplacement:
     def __init__(self, threshold=95, replacement=45, low_threshold=-100, low_replacement=-50,
-                 xy_search_in_voxels=10, z_search_in_voxels=2):
+                 xy_search_in_mm=4, z_search_in_slices=2):
 
-        self.xy_search = xy_search_in_voxels
-        self.z_search = z_search_in_voxels
+        self.xy_search = xy_search_in_mm
+        self.z_search = z_search_in_slices
         self.threshold = threshold
         self.replacement = replacement
         self.low_threshold = low_threshold
         self.low_replacement = low_replacement
 
     def apply_str_to_seed_locations(self, ctdata: CTdata, seed_locations: List[List[float]]):
+        # this assumes pixels are square
+        xy_search_in_voxels = int((self.xy_search / 10.) / ctdata.voxel_size_in_cm()[0])
         for (i, location) in enumerate(seed_locations):
             i, j, k = voxelnav.get_ijk_from_xyz(location, ctdata.bounds)
             self.check_indices_for_out_of_bounds_search(ctdata, i, j, k)
             for delta_z in range(-self.z_search, self.z_search + 1):
-                for delta_y in range(-self.xy_search, self.xy_search):
-                    for delta_x in range(-self.xy_search, self.xy_search):
-                        if ctdata.image[(i+delta_x, j+delta_y, k+delta_z)] > self.threshold:
-                            ctdata.image[(i + delta_x, j + delta_y, k + delta_z)] = self.replacement
+                for delta_y in range(-xy_search_in_voxels, xy_search_in_voxels):
+                    for delta_x in range(-xy_search_in_voxels, xy_search_in_voxels):
+                        distance_in_voxels = math.sqrt(delta_x**2 + delta_y**2)
+                        if distance_in_voxels < xy_search_in_voxels:
+                            if ctdata.image[(i+delta_x, j+delta_y, k+delta_z)] > self.threshold:
+                                ctdata.image[(i + delta_x, j + delta_y, k + delta_z)] = self.replacement
+                            else:
+                                pass
                         else:
                             pass
 
