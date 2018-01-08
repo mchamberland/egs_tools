1ac250da40fe7abefbaae1ac9ea3c2a9c80d32b7
Marc Chamberland <mchamberland@mac.com>
2018-01-07 20:00:26 -0800

    Add DICOM UID generator
    Former-commit-id: 792c8759b4bcffca0e982ffe274044a70f32ebd1
    Former-commit-id: 03fbbe009388a398b314b6bd88ed108c88697857
    Former-commit-id: ba68378ecac2952c707d7cdee29417238dadfb52

---
+++ dicom/uid.py
@@ -0,0 +1,29 @@
+import os
+import datetime as dt
+
+ROOT_UID = "1.2.124"
+
+
+class DicomUIDGenerator:
+    def __init__(self, root=None):
+        if root:
+            self.root = root
+        else:
+            self.root = ROOT_UID
+        self.uid_counter = 0
+
+    def create_series_instance_uid(self):
+        return self.create_uid(self.root + '.1.3')
+
+    def create_sop_instance_uid(self):
+        return self.create_uid(self.root + '1.4')
+
+    def create_study_instance_uid(self):
+        return self.create_uid(self.root + '1.2')
+
+    def create_uid(self, uid):
+        uid = uid + '.' + str(os.getpid())
+        now = dt.datetime.strftime(dt.datetime.today(), "%Y%m%d%H%M%f")
+        uid = uid + '.' + now
+        self.uid_counter += 1
+        return uid + '.' + str(self.uid_counter)
