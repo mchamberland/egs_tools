7bcf0f078145e9a72dba92ee70a3fb991bd8c9b0
Marc Chamberland <mchamberland@mac.com>
2018-05-08 21:25:41 -0700

    Exclude overlap area when creating egsphant
    If a single structure has multiple contours that overlap
    on the same slice, then it is probably a donut-shaped
    structure and the overlap between the contours should be
    excluded from the contour mask.
    
    
    Former-commit-id: 7c5466077a17ce314c94a9af3e80417318985775
    Former-commit-id: 98325b6bcab3ae094117e3a8c9464e28b09d835c
    Former-commit-id: 3237565036479cc7633b28979bfcf84d9fa3aa0e

--- ct_tools/ct_to_egsphant.py
+++ ct_tools/ct_to_egsphant.py
@@ -151,8 +151,8 @@ def create_contour_masks(ctdata, contour_path_dict):
                 temp_cumulative_mask = np.zeros(ctdata.dimensions[0:2], dtype=bool, order='F')
                 for path in path_data[k]:
                     temp_mask = path.contains_points(ctdata.pixel_centre_coordinates)
-                    temp_cumulative_mask = temp_cumulative_mask | temp_mask.reshape(ctdata.dimensions[0:2],
-                                                                                    order='F')
+                    temp_cumulative_mask = np.logical_xor(temp_cumulative_mask,
+                                                          temp_mask.reshape(ctdata.dimensions[0:2], order='F'))
                 mask_array[:, :, k] = temp_cumulative_mask
         contour_mask_dict[name] = mask_array
         total_cumulative_mask = total_cumulative_mask | mask_array
